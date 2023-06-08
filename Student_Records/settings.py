cbe81da9d6354a56163819264de4df855a365bf4
kefeletswe <kefeletswenonhlanhla@gmail.com>
2023-06-08 16:22:07 +0000

    create heroku app, new database and create an envy file thrn add a secret key, comment out databases and migrate

--- Student_Records/settings.py
+++ Student_Records/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^%)6z362-03svn+(f50g+bfvkfwd3kz!u_r_!v29sm@a0!(9h^'
+SECRET_KEY = os.environ.get('my_super^secret@key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-kefeletswe-athleticssco-qjmp1gbw5zp.ws-eu99.gitpod.io']
 
 
 # Application definition
@@ -37,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Scores_Board',
+    'Score_Board',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +78,15 @@ WSGI_APPLICATION = 'Student_Records.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
---
+++ envy.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"]="postgres://jwuabtxz:0dpAygX4hi8RMU3AgUG_33p95Iqw1g0x@lucky.db.elephantsql.com/jwuabtxz"
+os.environ["SECRET_KEY"]="my_super^secret@key"
